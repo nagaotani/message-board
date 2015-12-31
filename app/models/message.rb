39fc2c2cbb34334f66db1c8d830f749c9244bb04
nagaotani <nagayasu.otani@gmail.com>
2015-12-31 01:55:42 +0000

    Added age item

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,13 @@ class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20}, presence: true
     # 内容は必須入力かつ２文字以上３０文字以下
     validates :body, length:{minimum: 2, maximum: 30}, presence: true
+    # 年齢は必須入力かつ0以上
+    validates :age, numericality:{ greater_than: 0 }, presence: true
+    
+    def age=(value)
+        value.tr!('０-９', '0-9') if value.is_a?(String)
+        super(value)
+    end
+    
+    
 end

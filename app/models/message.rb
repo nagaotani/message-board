62f62cf0ef9a3fed54bdab94574609f3ae1693cb
nagaotani <nagayasu.otani@gmail.com>
2016-01-09 09:41:59 +0000

    fixed age equal_to and only integer

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ActiveRecord::Base
     # 内容は必須入力かつ２文字以上３０文字以下
     validates :body, length:{minimum: 2, maximum: 30}, presence: true
     # 年齢は必須入力かつ0以上
-    validates :age, numericality:{ greater_than: 0 }, presence: true
+    validates :age, numericality:{ greater_than_or_equal_to: 0, only_integer: true }, presence: true
     
     def age=(value)
         value.tr!('０-９', '0-9') if value.is_a?(String)
